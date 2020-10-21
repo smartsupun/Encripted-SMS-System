36245062e252e8498c9904173ee8f01c73e90633
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2020-10-21 18:24:44 +0530

    Update DbHandler.java

--- app/src/main/java/com/example/app3/DbHandler.java
+++ app/src/main/java/com/example/app3/DbHandler.java
@@ -9,24 +9,24 @@ import android.widget.EditText;
 import androidx.annotation.Nullable;
 
 public class DbHandler extends SQLiteOpenHelper {
-    private static final int VERSION = 2;
-    private static final String DB_NAME = "sms";
-    private static final String TABLE_NAME1 = "login";
-    private static final String TABLE_NAME2 = "chat";
+    public static final int VERSION = 2;
+    public static final String DB_NAME = "sms.db";
+    public static final String TABLE_NAME1 = "chat";
+    //public static final String TABLE_NAME2 = "";
 
     //collumn name table1
-    private static final String USERID = "user_id";
-    private static final String NAME = "Name";
-    private static final String PHONENUMBER = "phonenumber";
-    private static final String PASSWORD = "password";
+    public static final String USERID = "user_id";
+    //public static final String NAME = "Name";
+    public static final String PHONENUMBER = "phoneNo";
+    //public static final String PASSWORD = "password";
 
     //collumn name table2
-    private static final String MESSAGEID = "message_id";
-    private static final String SENDERID = "sender_id";
-    private static final String RECIEVERID = "reciever_id";
-    private static final String MESSAGE = "message";
-    private static final String DATE = "date";
-    private static final String TIME = "time";
+    //public static final String MESSAGEID = "message_id";
+   // public static final String SENDERID = "sender_id";
+    //public static final String RECIEVERID = "reciever_id";
+    public static final String MESSAGE = "msg";
+    //public static final String DATE = "date";
+    //public static final String TIME = "time";
 
     public DbHandler(@Nullable Context context) {
         super(context, DB_NAME , null ,  VERSION );
@@ -34,35 +34,37 @@ public class DbHandler extends SQLiteOpenHelper {
 
     //create table
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-          String TABLE_CREATE_QUERY1= "CREATE TABLE " + TABLE_NAME1 + "" +"("
-                  +USERID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
-                  +NAME + " TEXT,"
+    public void onCreate(SQLiteDatabase db) {
+          /* +USERID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                 +NAME + " TEXT,"
                   +PHONENUMBER + " TEXT,"
-                  +PASSWORD + " TEXT,"+
-                  ");";
+                 +PASSWORD + " TEXT,"+  */
+        String TABLE_CREATE_QUERY1= String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT,%s TEXT, ) ;", TABLE_NAME1, USERID, PHONENUMBER, MESSAGE);
           //run table create query
-        sqLiteDatabase.execSQL(TABLE_CREATE_QUERY1);
+      db.execSQL(TABLE_CREATE_QUERY1);
 
-        Object user_id;
+        /*Object user_id;
         String TABLE_CREATE_QUERY2= String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY AUTOINCREMENT,%s INTEGERREFERENCESTABLE_NAME1(USERID),%s INTEGERREFERENCESTABLE_NAME1(USERID),%s TEXT,%s DATE,%s TIME,);", TABLE_NAME2, MESSAGEID, SENDERID, RECIEVERID, MESSAGE, DATE, TIME);
         //run table create query
-        sqLiteDatabase.execSQL(TABLE_CREATE_QUERY2);
+       db.execSQL(TABLE_CREATE_QUERY2);  */
     }
+
+
 
     //version update
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
            String DROP_TABLE_QUERY1="DROP TABLE IF EXISTS "+TABLE_NAME1;
            //drop older table if executed
-           sqLiteDatabase.execSQL(DROP_TABLE_QUERY1);
+           db.execSQL(DROP_TABLE_QUERY1);
            //create table again
-           onCreate(sqLiteDatabase);
+           onCreate(db);
 
-        String DROP_TABLE_QUERY2="DROP TABLE IF EXISTS "+TABLE_NAME2;
+       /* String DROP_TABLE_QUERY2="DROP TABLE IF EXISTS "+TABLE_NAME2;
         //drop older table if executed
-        sqLiteDatabase.execSQL(DROP_TABLE_QUERY1);
+        db.execSQL(DROP_TABLE_QUERY2);
         //create table again
-        onCreate(sqLiteDatabase);
+        onCreate(db);  */
     }
 }
+
