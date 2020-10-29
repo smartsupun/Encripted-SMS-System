fac7ace0049c5628e2ff0be5ebc078e4c8b42663
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2020-10-29 14:15:10 +0530

    Update DbHandler.java

--- app/src/main/java/com/example/app3/DbHandler.java
+++ app/src/main/java/com/example/app3/DbHandler.java
@@ -9,24 +9,16 @@ import android.widget.EditText;
 import androidx.annotation.Nullable;
 
 public class DbHandler extends SQLiteOpenHelper {
-    public static final int VERSION = 2;
-    public static final String DB_NAME = "sms.db";
+    public static final int VERSION = 1;
+    public static final String DB_NAME = "chat.db";
     public static final String TABLE_NAME1 = "chat";
-    //public static final String TABLE_NAME2 = "";
+  
 
-    //collumn name table1
+    //collumn name table
     public static final String USERID = "user_id";
-    //public static final String NAME = "Name";
     public static final String PHONENUMBER = "phoneNo";
-    //public static final String PASSWORD = "password";
-
-    //collumn name table2
-    //public static final String MESSAGEID = "message_id";
-   // public static final String SENDERID = "sender_id";
-    //public static final String RECIEVERID = "reciever_id";
     public static final String MESSAGE = "msg";
-    //public static final String DATE = "date";
-    //public static final String TIME = "time";
+    
 
     public DbHandler(@Nullable Context context) {
         super(context, DB_NAME , null ,  VERSION );
@@ -35,18 +27,12 @@ public class DbHandler extends SQLiteOpenHelper {
     //create table
     @Override
     public void onCreate(SQLiteDatabase db) {
-          /* +USERID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
-                 +NAME + " TEXT,"
-                  +PHONENUMBER + " TEXT,"
-                 +PASSWORD + " TEXT,"+  */
-        String TABLE_CREATE_QUERY1= String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT,%s TEXT, ) ;", TABLE_NAME1, USERID, PHONENUMBER, MESSAGE);
+          
+         String TABLE_CREATE_QUERY= "CREATE TABLE " + TABLE_NAME + "(" + USERID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PHONENUMBER +  " TEXT, " + MESSAGE + " TEXT)";
           //run table create query
       db.execSQL(TABLE_CREATE_QUERY1);
 
-        /*Object user_id;
-        String TABLE_CREATE_QUERY2= String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY AUTOINCREMENT,%s INTEGERREFERENCESTABLE_NAME1(USERID),%s INTEGERREFERENCESTABLE_NAME1(USERID),%s TEXT,%s DATE,%s TIME,);", TABLE_NAME2, MESSAGEID, SENDERID, RECIEVERID, MESSAGE, DATE, TIME);
-        //run table create query
-       db.execSQL(TABLE_CREATE_QUERY2);  */
+       
     }
 
 
@@ -60,11 +46,7 @@ public class DbHandler extends SQLiteOpenHelper {
            //create table again
            onCreate(db);
 
-       /* String DROP_TABLE_QUERY2="DROP TABLE IF EXISTS "+TABLE_NAME2;
-        //drop older table if executed
-        db.execSQL(DROP_TABLE_QUERY2);
-        //create table again
-        onCreate(db);  */
+       
     }
 }
 
