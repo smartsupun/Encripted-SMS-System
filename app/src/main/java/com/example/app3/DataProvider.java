405df34c315e7a6d5a5a8a70c5070f9d5559e3be
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2020-11-04 13:27:50 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/app3/DataProvider.java
@@ -0,0 +1,12 @@
+package com.example.app3;
+
+public class DataProvider {
+    public boolean position;
+    public String message;
+    public DataProvider(boolean position,String message){
+        super();
+        this.position=position;
+        this.message=message;
+    }
+
+}
