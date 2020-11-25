2784d8118e8758f7db82170da9746c1e626f85b5
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2020-11-25 11:30:26 +0530

    Create ChatMessage.java

---
+++ app/src/main/java/com/example/app3/ChatMessage.java
@@ -0,0 +1,11 @@
+package com.example.app3;
+public class ChatMessage {
+	public boolean left;
+	public String message;
+
+	public ChatMessage(boolean left, String message) {
+		super();
+		this.left = left;
+		this.message = message;
+	}
+}
